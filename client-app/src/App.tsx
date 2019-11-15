a76d5347de9e23641e2a4710f62ea77bdddbd895
6895mahfuzgit <6895mahfuz@gmail.com>
2019-11-15 11:34:52 +0600

    formated code in client app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,56 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { Header, Icon } from 'semantic-ui-react'
+import { Header, Icon, List } from 'semantic-ui-react'
 
 class App extends Component {
 
- state={
-   values:[]
- }
+  state = {
+    values: []
+  }
 
- componentDidMount(){
-
-  axios.get('http://localhost:5000/api/values').then((response)=>{
-    this.setState({
-      values:response.data
-    })
-  });
-}
-
-//   this.setState({
-//     values:[
-//      {id:1,name:'Value 101'},
-//      {id:2,name:'Value 102'},
-//      {id:3,name:'Value 103'},
-//      {id:4,name:'Value 104'},
-//      {id:5,name:'Value 105'},
-//     ]
-//   })
-//  }
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values').then((response) => {
+      this.setState({
+        values: response.data
+      })
+    });
+  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
-          <Header as='h2'>
-    <Icon name='plug' />
-    <Header.Content>Uptime Guarantee</Header.Content>
-  </Header>
-          <ul>{
-             this.state.values.map((value:any)=>(
-              <li key={value.id} >{value.name}</li>
-             ))
-           }  
-           </ul>        
-        </header>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>React ASP.NET Core</Header.Content>
+        </Header>
+
+        <List>
+          {
+            this.state.values.map((value: any) => (
+              <List.Item key={value.id}>{value.name}</List.Item>
+            ))
+          }
+        </List>
       </div>
     );
-
   }
 }
-
 
 export default App;

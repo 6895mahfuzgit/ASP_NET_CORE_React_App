59bd12f5e7ada6dfeb7034cc6d92d7896a6c2050
6895mahfuzgit <6895mahfuz@gmail.com>
2019-11-15 21:43:17 +0600

    Added Activity controller and List method to get all activity

---
+++ ReactPracProjectApp/Controllers/ActivitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReactPracProjectApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await _mediator.Send(new List.Query());
+        }
+    }
+}
